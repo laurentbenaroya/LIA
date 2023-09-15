5ca31a8f843467638d8b33a1ae2f9a5155dc8c00
benaroya <laurent.benaroya@gmail.com>
2023-09-15 18:59:53 +0200

    rewrite everything!

--- evaluation.py
+++ evaluation.py
@@ -7,45 +7,9 @@ import torchvision
 import os
 from tqdm import tqdm
 import torchvision.transforms as transforms
-from dataset import Vox256_eval, Taichi_eval, TED_eval
+from dataset import Vox256_eval, Taichi_eval, TED_eval, Macron
 from torch.utils import data
-from PIL import Image
 import lpips
-
-
-def data_sampler(dataset, shuffle):
-    if shuffle:
-        return data.RandomSampler(dataset)
-    else:
-        return data.SequentialSampler(dataset)
-
-
-def sample_data(loader):
-    while True:
-        for batch in loader:
-            yield batch
-
-
-def load_image(filename, size):
-    img = Image.open(filename).convert('RGB')
-    img = img.resize((size, size))
-    img = np.asarray(img)
-    img = np.transpose(img, (2, 0, 1))  # 3 x 256 x 256
-
-    return img / 255.0
-
-
-def save_video(save_path, name, vid_target_recon, fps=10.0):
-    vid = (vid_target_recon.permute(0, 2, 3, 4, 1) * 127.5 + 128).clamp(0, 255).to(torch.uint8)
-    torchvision.io.write_video(save_path + '%s.mp4' % name, vid[0].cpu(), fps=fps)
-
-
-def data_preprocessing(img_path, size):
-    img = load_image(img_path, size)  # [0, 1]
-    img = torch.from_numpy(img).unsqueeze(0).float()  # [0, 1]
-    imgs_norm = (img - 0.5) * 2.0  # [-1, 1]
-
-    return imgs_norm
 
 
 class Eva(nn.Module):
@@ -69,10 +33,13 @@ class Eva(nn.Module):
         elif args.dataset == 'ted':
             path = 'checkpoints/ted.pt'
             dataset = TED_eval(transform)
+        elif args.dataset == 'macron':
+            path = 'exps/v1/checkpoint/806000.pt'
+            # path = 'checkpoints/vox.pt'
+            macron_rootdir = '/mnt/ddr/data/Macron/'
+            dataset = Macron(macron_rootdir, 'test', transform)
         else:
             raise NotImplementedError
-
-        os.makedirs(os.path.join(self.save_path, args.dataset), exist_ok=True)
 
         print('==> loading model')
         self.gen = Generator(args.size, args.latent_dim_style, args.latent_dim_motion, args.channel_multiplier).cuda()
@@ -94,26 +61,16 @@ class Eva(nn.Module):
 
         loss_list = []
         loss_lpips = []
-        for idx, (vid_name, vid) in tqdm(enumerate(self.loader)):
+        for _ in range(10):  # repeat random loader
+            for img_source, img_target in tqdm(self.loader):
 
-            with torch.no_grad():
-
-                vid_real = []
-                vid_recon = []
-                img_source = vid[0].cuda()
-                for img_target in vid:
+                with torch.no_grad():
                     img_target = img_target.cuda()
+                    img_source = img_source.cuda()
                     img_recon = self.gen(img_source, img_target)
-                    vid_recon.append(img_recon.unsqueeze(2))
-                    vid_real.append(img_target.unsqueeze(2))
 
-                vid_recon = torch.cat(vid_recon, dim=2)
-                vid_real = torch.cat(vid_real, dim=2)
-
-                loss_list.append(torch.abs(0.5 * (vid_recon.clamp(-1, 1) - vid_real)).mean().cpu().numpy())
-                vid_real = vid_real.permute(0, 2, 1, 3, 4).squeeze(0)
-                vid_recon = vid_recon.permute(0, 2, 1, 3, 4).squeeze(0)
-                loss_lpips.append(self.loss_fn.forward(vid_real, vid_recon.clamp(-1, 1)).mean().cpu().detach().numpy())
+                    loss_list.append(torch.abs(0.5 * (img_recon.clamp(-1, 1) - img_target)).mean().cpu().numpy())
+                    loss_lpips.append(self.loss_fn(img_target, img_recon.clamp(-1, 1)).mean().cpu().detach().numpy())
 
         print("reconstruction loss: %s" % np.mean(loss_list))
         print("lpips loss: %s" % np.mean(loss_lpips))
@@ -122,12 +79,11 @@ class Eva(nn.Module):
 if __name__ == '__main__':
     # training params
     parser = argparse.ArgumentParser()
-    parser.add_argument("--channel_multiplier", type=int, default=1)
-    parser.add_argument("--size", type=int, default=256)
-    parser.add_argument("--latent_dim_style", type=int, default=512)
-    parser.add_argument("--latent_dim_motion", type=int, default=20)
-    parser.add_argument("--dataset", type=str, choices=['vox', 'taichi', 'ted'], default='')
-    parser.add_argument("--save_path", type=str, default='./evaluation_res')
+    parser.add_argument("--channel_multiplier", type=int, default=1, help=' %(default)s')
+    parser.add_argument("--size", type=int, default=256, help=' %(default)s')
+    parser.add_argument("--latent_dim_style", type=int, default=512, help=' %(default)s')
+    parser.add_argument("--latent_dim_motion", type=int, default=20, help=' %(default)s')
+    parser.add_argument("--dataset", type=str, choices=['vox', 'taichi', 'ted', 'macron'], required=True, help=' %(default)s')
     args = parser.parse_args()
 
     demo = Eva(args)
